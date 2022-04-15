48b8723da591ee8f0425a74c91260f9f117bbb8b
Deepanshu <iamdeepanshu9@gmail.com>
2022-04-15 16:52:22 +0530

    understand the ViewChild and the ngAfterViewInit

--- app/auth-form/auth-form.component.ts
+++ app/auth-form/auth-form.component.ts
@@ -1,7 +1,7 @@
-import {Component, Output, EventEmitter, AfterContentInit,QueryList, ContentChildren} from '@angular/core';
+import {Component, Output, EventEmitter,ViewChild,AfterViewInit,AfterContentInit,QueryList, ContentChildren} from '@angular/core';
 
 import { AuthRememberComponent } from './auth-remember.component';
-
+import {AuthMessageComponent} from "./auth-message.component";
 import { User } from './auth-form.interface';
 
 @Component({
@@ -19,23 +19,32 @@ import { User } from './auth-form.interface';
           <input type="password" name="password" ngModel>
         </label>
         <ng-content select="auth-remember"></ng-content>
-        <div *ngIf="showMessage">
-          You will be logged in for 30 days
-        </div>
+        <auth-message [style.display]="(showMessage ? 'inherit' : 'none')"></auth-message>
         <ng-content select="button"></ng-content>
       </form>
     </div>
   `
 })
-export class AuthFormComponent implements AfterContentInit {
+export class AuthFormComponent implements AfterContentInit,AfterViewInit {
 
   showMessage: boolean;
 
   @ContentChildren(AuthRememberComponent) remember:QueryList<AuthRememberComponent>;
 
+  @ViewChild(AuthMessageComponent) message : AuthMessageComponent;
+
   @Output() submitted: EventEmitter<User> = new EventEmitter<User>();
 
+  ngAfterViewInit() {
+    // this.message.days = 30; // give error of inline changing that cant change after view init
+  }
+
   ngAfterContentInit() {
+    if(this.message)
+    {
+      this.message.days = 90;
+    }
+
     if (this.remember) {
       this.remember.forEach((item)=> {
         item.checked.subscribe((checked: boolean) => this.showMessage = checked);
